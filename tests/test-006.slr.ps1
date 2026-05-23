310570551851541bfc90213405e856242b33fb54
Paul Higgs <paul_higgs@hotmail.com>
2026-05-24 05:37:28 +0800

    move A177 SLR examples

---
+++ tests/test-006.slr.ps1
@@ -0,0 +1,9 @@
+# Tests for Examples in the A177 specification
+
+[string[]]$test_files = @()
+Get-ChildItem -Path "$PSScriptRoot\..\input\test-006\SLR" -Filter *.xml | 
+	Foreach-Object {	
+		$test_files += $_.FullName
+	}
+
+node $PSScriptRoot\..\test-runner.js --mode slr --nomarkup --src $test_files
