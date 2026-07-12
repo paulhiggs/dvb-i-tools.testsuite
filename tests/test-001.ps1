1de9673266b9f1aa36b7efdbaae9104e3b87739d
Paul Higgs <paul_higgs@hotmail.com>
2026-07-12 10:12:42 +0100

    update regular expression testing

--- tests/test-001.ps1
+++ tests/test-001.ps1
@@ -1,3 +1,5 @@
 node $PSScriptRoot\..\input\test-001\test-re1.js
 node $PSScriptRoot\..\input\test-001\test-re2.js
 node $PSScriptRoot\..\input\test-001\test-lang-re.js
+
+node $PSScriptRoot\..\input\test-001\test-re3.js
